36d30ca2b1f38513bed3e827c87ab30b52df56e4
Sayuru De Alwis <75390567+Sayuru99@users.noreply.github.com>
2021-07-26 11:12:38 +0530

    Add boss level challenge files

---
+++ index.js
@@ -0,0 +1,33 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//player 1 winds
+if (randomNumber1 > randomNumber2) {
+
+  document.querySelector("h1").innerHTML = " Klaus Wins!";
+}
+
+else if (randomNumber2 > randomNumber1) {
+document.querySelector("h1").innerHTML = "Elijah Wins! "
+}
+
+else {
+  document.querySelector("h1").innerHTML = "Rebecca comes!!!";
+}
